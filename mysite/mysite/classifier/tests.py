d6b1306fc9531e0077c842d5873ef6d7a658fdda
Kieran Austin <51318680+kieranaustin97@users.noreply.github.com>
2021-01-06 12:48:50 +0000

    Update tests.py
    Start editing tests.py

--- mysite/mysite/classifier/tests.py
+++ mysite/mysite/classifier/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
+#Change to import one at a time, in order of test usage
+from mysite.mysite.classifier.scripts.website_classification import *
+
+
 # Create your tests here.
