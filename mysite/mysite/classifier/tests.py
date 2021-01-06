15cd0ab96938be199464d9823af258c6fdf103f0
Kieran Austin <51318680+kieranaustin97@users.noreply.github.com>
2021-01-06 12:50:29 +0000

    Update tests.py
    Outline tests to be written

--- mysite/mysite/classifier/tests.py
+++ mysite/mysite/classifier/tests.py
@@ -5,3 +5,13 @@ from mysite.mysite.classifier.scripts.website_classification import *
 
 
 # Create your tests here.
+
+## Tests for website_classification (Must)
+
+## Tests for models (Maybe, currently no Django stored models, This may be changed when storing additional user classifications)
+
+## Tests for views (Should)
+
+## Tests for forms (Should)
+
+## Tests for user experience (Maybe)
