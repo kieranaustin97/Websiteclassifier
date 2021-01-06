fa4d79d45807e787abe4f2cfb88616b9c806b49c
Kieran Austin <51318680+kieranaustin97@users.noreply.github.com>
2021-01-06 16:59:04 +0000

    Update tests.py
    Add first test

--- mysite/mysite/classifier/tests.py
+++ mysite/mysite/classifier/tests.py
@@ -1,12 +1,21 @@
 from django.test import TestCase
 
 #Change to import one at a time, in order of test usage
-from mysite.mysite.classifier.scripts.website_classification import *
+from classifier.scripts.website_classification import (clean_html_text)
 
 
 # Create your tests here.
 
 ## Tests for website_classification (Must)
+### Only test methods written not imported such as requests library functions
+class WebsiteClassificationMethods(TestCase):
+    def test_clean_html_text(self):
+        expected_clean_string = "this is the correctly cleaned string"
+        input_html_string = "This is the \n correctly cleaned string"
+        cleaned_string = clean_html_text(input_html_string)
+
+        self.assertEqual(cleaned_string, expected_clean_string)
+
 
 ## Tests for models (Maybe, currently no Django stored models, This may be changed when storing additional user classifications)
 
