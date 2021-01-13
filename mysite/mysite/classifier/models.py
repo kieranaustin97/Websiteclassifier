f62681a3e9ad2d6632f836e47c357971b13048ac
Kieran Austin <51318680+kieranaustin97@users.noreply.github.com>
2021-01-13 18:19:17 +0000

    Update models.py
    Change model order

--- mysite/mysite/classifier/models.py
+++ mysite/mysite/classifier/models.py
@@ -3,5 +3,6 @@ from django.db.models import CharField
 
 # Create your models here.
 class PredictedWebsites(models.Model):
-    classification = CharField(max_length=50)
     url = CharField(max_length=200)
+    classification = CharField(max_length=50)
+    
