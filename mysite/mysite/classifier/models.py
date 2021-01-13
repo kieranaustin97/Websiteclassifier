8ae0154068aa750100f9549af9864dd015a56cb6
Kieran Austin <51318680+kieranaustin97@users.noreply.github.com>
2021-01-13 14:49:32 +0000

    Update models.py
    Added PredictedWebsites model

--- mysite/mysite/classifier/models.py
+++ mysite/mysite/classifier/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.db.models import CharField
 
 # Create your models here.
+class PredictedWebsites(models.Model):
+    classification = CharField(max_length=50)
+    url = CharField(max_length=200)
