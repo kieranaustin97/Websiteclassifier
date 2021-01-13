f550f9534174e40d66e6ecb61c5e82e689712805
Kieran Austin <51318680+kieranaustin97@users.noreply.github.com>
2021-01-13 11:52:40 +0000

    modified tests

--- mysite/mysite/classifier/tests.py
+++ mysite/mysite/classifier/tests.py
@@ -1,16 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, mock
 from unittest.mock import patch
 
 #Change to import one at a time, in order of test usage
-import classifier.scripts.website_classification as web_classifier
 
+from classifier.scripts import website_classification
 # Create your tests here.
 
 ## Tests for website_classification (Must)
 ### Only test methods written not imported such as requests library functions
 class WebsiteClassificationMethods(TestCase):
 
-    testWebsiteClassifierObject = web_classifier.WebsiteClassifier()
+    testWebsiteClassifierObject = website_classification.WebsiteClassifier()
  
     def test_clean_html_text(self):
         input_html_string = "This is the \n correctly cleaned string"
@@ -19,12 +19,18 @@ class WebsiteClassificationMethods(TestCase):
         expected_clean_string = "this is the correctly cleaned string"
         self.assertEqual(cleaned_string, expected_clean_string)
 
-    #def test_class_collect_website(self):
-    #       
-    #    test_class_name = "test_site"
-    #    test_url = "url"
-    #    classifed_message_data = self.testWebsiteClassifierObject.class_collect_website(test_class_name,test_url)
+    #@mock.patch('classifier.scripts.website_classification.clean_html_text', return_value="output_string")
+    #def test_class_collect_website(self, mock_clean_html):
+   #        
+  #      test_class_name = "test_site"
+ #       test_url = "url"
+#
+ #       classifed_message_data = self.testWebsiteClassifierObject.class_collect_website(test_class_name,test_url)
+#
+    #    expected_classified_data = "fred"
+    #    self.assertEqual(classifed_message_data, expected_classified_data)
 
+    
 
 
 ## Tests for models (Maybe, currently no Django stored models, This may be changed when storing additional user classifications)
