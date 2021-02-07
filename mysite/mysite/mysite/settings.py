47576a50dbf39b4c26f09ca0fdb5f7621865cbee
Kieran Austin <51318680+kieranaustin97@users.noreply.github.com>
2021-02-07 12:31:24 +0000

    Use materializecss django forms to improve look

--- mysite/mysite/mysite/settings.py
+++ mysite/mysite/mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'classifier'
+    'classifier',
+    'materializecssform',
 ]
 
 MIDDLEWARE = [
