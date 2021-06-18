25d93f2d5abe600918f5799165ee592743ea0825
Kieran Austin <51318680+kieranaustin97@users.noreply.github.com>
2021-06-18 10:53:04 +0100

    Improve readability of login view

--- mysite/mysite/classifier/views.py
+++ mysite/mysite/classifier/views.py
@@ -19,9 +19,9 @@ def loginPage(request):
     context = {}
     if request.method == 'POST':
         username = request.POST.get('username')
-        password =request.POST.get('password')
+        password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
-
+        
         if user is not None:
             login(request, user)
             return redirect('index')
